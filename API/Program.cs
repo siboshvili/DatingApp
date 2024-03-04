bf49b7d6fc28c9ff6d05e0339f49b4317da2d590
siboshvili <siboshvili@gmail.com>
2024-03-04 12:13:21 +0400

    error controller, middleware, testing

--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,9 @@
-using System.Text;
-using API.Data;
 using API.Exstensions;
-using API.Interface;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using API.Middleware;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +16,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
