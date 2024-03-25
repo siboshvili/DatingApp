0e93a05efaa65b23f882d529eaf28b76a11b8465
siboshvili <siboshvili@gmail.com>
2024-03-25 16:59:41 +0400

    Setting up the entities for messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -30,6 +30,17 @@ namespace API.Data
                 .WithMany(l => l.LikeByUsers)
                 .HasForeignKey(s => s.TargetUserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessageReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessageSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
