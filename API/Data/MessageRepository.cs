417b3e4dfc6a1d5381533ceaaaf4c723289a0fba
siboshvili <siboshvili@gmail.com>
2024-03-26 15:54:40 +0400

    Deleting messages on the API

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -46,9 +46,9 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted = false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username && u.SenderDeleted = false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.RecipientDeleted = false && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -64,9 +64,9 @@ namespace API.Data
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
-                    m => m.RecipientUsername == currentUserName &&
+                    m => m.RecipientUsername == currentUserName && m.RecipientDeleted = false &&
                     m.SenderUsername == recipientUsername ||
-                    m.RecipientUsername == recipientUsername &&
+                    m.RecipientUsername == recipientUsername && m.SenderDeleted = false &&
                     m.RecipientUsername == currentUserName
                 ).OrderBy(m => m.MessageSent)
                 .ToListAsync();
