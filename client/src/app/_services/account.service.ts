def9c2902460e0d2569e9c829703e3f08a49d934
siboshvili <siboshvili@gmail.com>
2024-04-04 12:53:46 +0400

    Client side SignalR

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { map, BehaviorSubject } from 'rxjs';
 import { User } from '../_models/user';
 import { environment } from 'src/environments/environment.development';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,11 @@ export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
-  constructor(private http: HttpClient) {}
+
+  constructor(
+    private http: HttpClient,
+    private presenceService: PresenceService
+  ) {}
 
   login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
@@ -41,11 +46,13 @@ export class AccountService {
     Array.isArray(roles) ? (user.roles = roles) : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
+    this.presenceService.createHubConnection(user);
   }
 
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presenceService.stopHubConnection();
   }
 
   getDecodedToken(token: string) {
---
+++ client/src/app/_services/presence.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PresenceService {
+  hubUrl = environment.hubUrl;
+  private hubConnection?: HubConnection;
+
+  constructor(private toaster: ToastrService) {}
+
+  createHubConnection(user: User) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'presence', {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch((error) => console.log(error));
+
+    this.hubConnection.on('UserIsOnline', (username) => {
+      this.toaster.info(username + ' has connected');
+    });
+
+    this.hubConnection.on('UserIsOffline', (username) => {
+      this.toaster.info(username + ' has desconnected');
+    });
+  }
+
+  stopHubConnection() {
+    this.hubConnection?.stop().catch((error) => console.log(error));
+  }
+}
